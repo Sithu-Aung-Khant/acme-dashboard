5bbc44855e853254b9347b297e88ad4c9d142a8b
Sithu-thedev <sithu.sithuthedev@gmail.com>
2024-03-21 21:37:27 +0630

    Add credentials provider for form-based authentication and update callbacks in NextAuth config

--- auth.ts
+++ auth.ts
@@ -1,9 +1,37 @@
 import type { NextAuthConfig } from 'next-auth';
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+const credentialsConfig = CredentialsProvider({
+  name: 'Credentials',
+  credentials: {
+    username: {
+      label: 'User Name',
+    },
+    password: {
+      label: 'Password',
+      type: 'password',
+    },
+  },
+  async authorize(credentials) {
+    if (credentials.username === 'sk' && credentials.password === '123')
+      return {
+        name: 'Valid',
+      };
+    else return null;
+  },
+});
 
 const config = {
-  providers: [Google],
+  providers: [credentialsConfig, Google],
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl;
+      if (pathname.startsWith('/dashboard/')) return !!auth;
+      return true;
+    },
+  },
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
