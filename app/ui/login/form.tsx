c6de3fd2f8d20c95afcec637e2a993d5c21e2194
Sithu-thedev <sithu.sithuthedev@gmail.com>
2024-03-23 22:30:53 +0630

    Update form error text styling

--- app/ui/login/form.tsx
+++ app/ui/login/form.tsx
@@ -93,7 +93,7 @@ const FormLabel = React.forwardRef<
   return (
     <Label
       ref={ref}
-      className={cn(error && 'text-destructive', className)}
+      className={cn(error && 'text-red-500', className)}
       htmlFor={formItemId}
       {...props}
     />
@@ -156,7 +156,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn('text-destructive text-[0.8rem] font-medium', className)}
+      className={cn('text-[0.8rem] font-medium text-red-500', className)}
       {...props}
     >
       {body}
