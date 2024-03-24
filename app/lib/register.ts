20145c9a6337fdec3dc75603ba25fcccd485130a
Sithu-thedev <sithu.sithuthedev@gmail.com>
2024-03-24 19:02:10 +0630

    Add bcrypt for password hashing, Prisma adapter for Auth.js, and update User and Account models in Prisma schema

--- app/lib/register.ts
+++ app/lib/register.ts
@@ -1,15 +1,36 @@
-"use server";
+'use server';
 
-import * as z from "zod";
-
-import { RegisterSchema } from "@/schemas";
+import * as z from 'zod';
+import bcrypt from 'bcrypt';
+import { db } from './db';
+import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from './user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
-    return { error: "Invalid fields!" };
+    return { error: 'Invalid fields!' };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: 'Account successfully created!' };
 };
